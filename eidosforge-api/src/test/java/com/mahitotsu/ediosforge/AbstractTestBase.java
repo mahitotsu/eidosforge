77dc72e9cac181a7c091a2b1d8b6ba15def9fb2e
Akio Kanashiki <akio@mail.com>
2025-05-07 06:28:38 +0900

    modified

---
+++ eidosforge-api/src/test/java/com/mahitotsu/ediosforge/AbstractTestBase.java
@@ -0,0 +1,12 @@
+package com.mahitotsu.ediosforge;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public abstract class AbstractTestBase {
+    
+    @LocalServerPort
+    private int port;
+}
